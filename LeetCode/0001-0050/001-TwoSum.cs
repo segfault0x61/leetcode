ecda8743ebd8162f4fc7ef6b6c67db4ea2557c27
segfault0x61 <torresj1997@outlook.com>
2022-03-31 23:42:31 -0500

    Add project files.

---
+++ LeetCode/0001-0050/001-TwoSum.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode._0001_0050
+{
+    public class _001_TwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                    return new int[] { dic[nums[i]], i };
+                else
+                    dic[target - nums[i]] = i;
+            }
+
+            return new int[] { };
+        }
+    }
+}
